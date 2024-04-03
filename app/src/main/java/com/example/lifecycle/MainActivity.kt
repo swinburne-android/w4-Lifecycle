679e984df4f1ea9ab97990bdfa53572672f87d89
Reza Ghafari <rezaghp@gmail.com>
2024-04-03 14:57:12 +1100

    x

---
+++ app/src/main/java/com/example/lifecycle/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.lifecycle
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+
+/*
+ in logcat filter:
+  package: com.example.lifecycle  tag:Lifecycle
+
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("Lifecycle", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycle", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("Lifecycle", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycle", "onDestroy")
+    }
+}
+
